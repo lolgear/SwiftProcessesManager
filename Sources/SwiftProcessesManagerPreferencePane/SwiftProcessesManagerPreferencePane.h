9b1728f6bd7a689d6d6ece2e06b2076d03d74354
Dmitry Lobanov <gaussblurinc@gmail.com>
2021-06-13 19:36:08 +0300

    core: preference pane has been added.

---
+++ Sources/SwiftProcessesManagerPreferencePane/SwiftProcessesManagerPreferencePane.h
@@ -0,0 +1,12 @@
+//
+//  SwiftProcessesManagerPreferencePane.h
+//  SwiftProcessesManagerPreferencePane
+//
+//  Created by Dmitry Lobanov on 12.06.2021.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface SwiftProcessesManagerPreferencePane : NSPreferencePane
+
+@end
---
+++ Sources/SwiftProcessesManagerPreferencePane/SwiftProcessesManagerPreferencePane.m
@@ -0,0 +1,16 @@
+//
+//  SwiftProcessesManagerPreferencePane.m
+//  SwiftProcessesManagerPreferencePane
+//
+//  Created by Dmitry Lobanov on 12.06.2021.
+//
+
+#import "SwiftProcessesManagerPreferencePane.h"
+
+@implementation SwiftProcessesManagerPreferencePane
+
+- (void)mainViewDidLoad {
+    
+}
+
+@end
