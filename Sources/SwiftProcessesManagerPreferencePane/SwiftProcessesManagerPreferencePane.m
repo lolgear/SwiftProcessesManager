504d3c0dc75fb596c21ecf967e8a2ef78d2c200c
Dmitry Lobanov <gaussblurinc@gmail.com>
2021-06-14 15:06:30 +0300

    core: preference pane sdk has been integrated.

--- Sources/SwiftProcessesManagerPreferencePane/SwiftProcessesManagerPreferencePane.m
+++ Sources/SwiftProcessesManagerPreferencePane/SwiftProcessesManagerPreferencePane.m
@@ -6,11 +6,49 @@
 //
 
 #import "SwiftProcessesManagerPreferencePane.h"
+#import <CoreFoundation/CFPreferences.h>
+@import ProcessessManagerSettingsSDK;
+
+@interface SwiftProcessesManagerPreferencePane ()
+@property (strong, nonatomic, readwrite) IBOutlet NSButton *checkbox;
+@end
 
 @implementation SwiftProcessesManagerPreferencePane
 
 - (void)mainViewDidLoad {
+    /// Retrieve preferences.
+    __auto_type domain = (__bridge CFStringRef)@"org.opensource.SwiftProcessesManager";
+    __auto_type setting = (__bridge CFStringRef)@"ShouldDisplayOnlyMyOwnProcesses";
+    CFPropertyListRef value;
     
+    /* Initialize the checkbox */
+    value = CFPreferencesCopyAppValue(setting,  domain);
+    if (value && CFGetTypeID(value) == CFBooleanGetTypeID()) {
+        [self.checkbox setState:CFBooleanGetValue(value)];
+    } else {
+        [self.checkbox setState:NO];
+    }
+    
+    if (value) {
+        CFRelease(value);
+    }
 }
+
+- (IBAction)checkboxClicked:(id)sender {
+    __auto_type domain = (__bridge CFStringRef)@"org.opensource.SwiftProcessesManager";
+    __auto_type setting = (__bridge CFStringRef)@"ShouldDisplayOnlyMyOwnProcesses";
+    __auto_type notificationName = (__bridge CFStringRef)@"PreferencesDidChange";
+    if ( [sender state] ) {
+        CFPreferencesSetAppValue(setting, kCFBooleanTrue, domain);
+    }
+    else {
+        CFPreferencesSetAppValue(setting, kCFBooleanFalse, domain);
+    }
+    
+    /// And post notification
+    __auto_type center = CFNotificationCenterGetDistributedCenter();
+    CFNotificationCenterPostNotification(center, notificationName, domain, nil, true);
+}
+
 
 @end
